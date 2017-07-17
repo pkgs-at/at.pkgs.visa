cea0644beac999072fc10c1ddd1e633b9c522eea
pkgs-at <sotaro.suzuki@architector.jp>
2017-07-17 22:25:44 +0900

    Initial Commit

---
+++ csharp/InviniVision1000/ConnectForm.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Architector.Visa.InviniVision1000
+{
+    partial class ConnectForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox_instruments = new System.Windows.Forms.ComboBox();
+            this.button_refresh = new System.Windows.Forms.Button();
+            this.button_connect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox_instruments
+            // 
+            this.comboBox_instruments.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox_instruments.FormattingEnabled = true;
+            this.comboBox_instruments.Location = new System.Drawing.Point(12, 14);
+            this.comboBox_instruments.Name = "comboBox_instruments";
+            this.comboBox_instruments.Size = new System.Drawing.Size(278, 21);
+            this.comboBox_instruments.TabIndex = 0;
+            // 
+            // button_refresh
+            // 
+            this.button_refresh.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_refresh.Location = new System.Drawing.Point(296, 12);
+            this.button_refresh.Name = "button_refresh";
+            this.button_refresh.Size = new System.Drawing.Size(75, 23);
+            this.button_refresh.TabIndex = 1;
+            this.button_refresh.Text = "Refresh";
+            this.button_refresh.UseVisualStyleBackColor = true;
+            this.button_refresh.Click += new System.EventHandler(this.button_refresh_Click);
+            // 
+            // button_connect
+            // 
+            this.button_connect.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_connect.Location = new System.Drawing.Point(377, 12);
+            this.button_connect.Name = "button_connect";
+            this.button_connect.Size = new System.Drawing.Size(75, 23);
+            this.button_connect.TabIndex = 2;
+            this.button_connect.Text = "Connect";
+            this.button_connect.UseVisualStyleBackColor = true;
+            this.button_connect.Click += new System.EventHandler(this.button_connect_Click);
+            // 
+            // ConnectForm
+            // 
+            this.AcceptButton = this.button_connect;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 47);
+            this.Controls.Add(this.button_connect);
+            this.Controls.Add(this.button_refresh);
+            this.Controls.Add(this.comboBox_instruments);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ConnectForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Connect...";
+            this.Shown += new System.EventHandler(this.ConnectForm_Shown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox_instruments;
+        private System.Windows.Forms.Button button_refresh;
+        private System.Windows.Forms.Button button_connect;
+    }
+}
